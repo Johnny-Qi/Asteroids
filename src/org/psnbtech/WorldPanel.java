1d65b30c2ba86b1a58116f86e66fbb7456a42035
Jonathan <qi004@csusm.edu>
2025-11-23 17:00:27 -0800

    Max added tusk int

--- src/org/psnbtech/WorldPanel.java
+++ src/org/psnbtech/WorldPanel.java
@@ -30,6 +30,7 @@ public class WorldPanel extends JPanel {
 	 * The size of the world in pixels.
 	 */
 	public static final int WORLD_SIZE = 550;
+	int tusk = 5;
 	
 	/**
 	 * The font used for the large text.
